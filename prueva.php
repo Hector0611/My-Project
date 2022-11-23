b3e089c4c049d386899915ae6a05ab115fb180eb
Hector Armin Che Dzib <chehector@06gmail.com>
2022-11-23 15:25:31 -0600

    Mi primer commit

---
+++ prueva.php
@@ -0,0 +1,33 @@
+<?php
+require('somefile.php');
+$variable;
+$t;
+$suma;
+$booleano = true;
+
+echo "esto es un print, se muestra en pantalla";
+echo $verb;
+
+
+  switch($suma){
+    case 1:
+    $v1 = $variable + $v1;
+    break;
+    case 3:
+    $v2 = $variable + $v2;
+    break;
+    case 4:
+    $v3 = $variable;
+    break;
+    default:
+    $v4 = $variable;
+    break;
+  }
+
+  while($j<=$i){
+    $v1 = $variable + $v1;
+    echo "*&nbsp&nbsp";
+  }
+
+}
+?>
